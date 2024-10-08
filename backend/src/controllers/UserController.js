c3e2c2c0931c332da23f3a7e23994830497cbbca
igopereira1 <igopereira1@gmail.com>
2024-10-08 20:14:56 -0300

    feat: add Users controller with register method

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,14 @@
+const userService = require("../services/UserService");
+
+const registerUser = async (req, res) => {
+  try {
+    const user = await userService.registerUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  registerUser,
+};

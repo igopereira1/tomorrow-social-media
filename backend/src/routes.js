cbb6fedb2a65e8edea147c90fe109066558680fd
igopereira1 <igopereira1@gmail.com>
2024-10-08 20:17:26 -0300

    feat: add POST route for user registration

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("./controllers/UserController");
 const postController = require("./controllers/PostController");
+
+router.post("/register", userController.registerUser);
 
 router.post("/posts", postController.createPost);
 router.delete("/posts/:id", postController.deletePost);
